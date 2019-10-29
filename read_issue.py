581ac97ec6d9ffb152c6a2ec63bd43f9dd46a4da
Zeren Lu <zeren.lu@gmail.com>
2019-10-29 21:34:50 +0100

    structure of getting info from Jira

---
+++ read_issue.py
@@ -0,0 +1,9 @@
+//read links(clones/is cloned by) from issues and write the results in excel
+
+//log in using API
+
+//read the ticket from Excel which contains KEY-WORD
+
+//check the KEY of the ticket in Jira and store the link of "clones" in a cell of the same row
+
+//end the session
