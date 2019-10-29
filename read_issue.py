a2f2ccc2a9f31422017b6cfc3c1b333c0f48f0d9
Zeren Lu <zeren.lu@gmail.com>
2019-10-29 21:40:00 +0100

    do fill-up in EXCEL

--- read_issue.py
+++ read_issue.py
@@ -1,9 +1,11 @@
-//read links(clones/is cloned by) from issues and write the results in excel
+#read links(clones/is cloned by) from issues and write the results in excel
 
-//log in using API
+#log in using API
 
-//read the ticket from Excel which contains KEY-WORD
+#read the ticket from Excel which contains KEY-WORD
 
-//check the KEY of the ticket in Jira and store the link of "clones" in a cell of the same row
+#check the KEY of the ticket in Jira and store the link(issue-key and summary) of "clones" in a cell of the same row
 
-//end the session
+#end the session
+
+#search the issue-key from link in Excel and write the issue-key and summary which contains KEY-WORD in the same row
