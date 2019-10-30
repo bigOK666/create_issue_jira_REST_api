857abe522da47c34fce4dc8f03acaf7c1b3b923b
Zeren Lu <zeren.lu@gmail.com>
2019-10-31 00:21:01 +0100

    check key in jira

--- read_issue.py
+++ read_issue.py
@@ -5,8 +5,29 @@ from jira import JIRA
 options = {'server':'https....'}
 jira = JIRA(options, basic_auth=(config.USER, config.PASS)
 #read the ticket from Excel which contains KEY-WORD
+import openpyxl
 
-#check the KEY of the ticket in Jira and store the link(issue-key and summary) of "clones" in a cell of the same row
+wb = openpyxl.load_workbook("twolabels.xlsx")
+
+ws = wb[wb.sheetnames[0]]
+
+# go through all rows in a specific colum
+
+for cell in ws['C']:
+    issue_title_excel = cell.value
+    if "KEY-WORD" in issue_title_excel:
+        issue_key_excel = ws['B'.format(cell.row)]
+        if issue_key_excel != "key":
+            print(issue_key_excel)
+            issue = jira.issue(issue_key_excel, fields = "key, summary, issuelinks")
+            #check all issuelinks check the KEY of the ticket in Jira and store the link(issue-key and summary) of "clones" in a cell of the same row
+            for issue_link in issue.fields.issuelinks:
+                if issue_link.type == "Cloner" and hasattr(issue_link, "outwardIssue"):
+                    original_issue_key = issue_link.outwardIssue
+                    original_issue_title = issue_link.outwardIssue.fields.summary
+                    ws['D{}'.format(cell.row)] = original_issue_key
+                    ws['E{}'.format(cell.row)] = original_issue_title
+
 
 #end the session
 
