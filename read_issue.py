5a922341248b42864cc09752481494f4c1a44b28
Zeren Lu <zeren.lu@gmail.com>
2019-10-30 23:25:46 +0100

    log in to jira

--- read_issue.py
+++ read_issue.py
@@ -1,7 +1,9 @@
 #read links(clones/is cloned by) from issues and write the results in excel
 
 #log in using API
-
+from jira import JIRA
+options = {'server':'https....'}
+jira = JIRA(options, basic_auth=(config.USER, config.PASS)
 #read the ticket from Excel which contains KEY-WORD
 
 #check the KEY of the ticket in Jira and store the link(issue-key and summary) of "clones" in a cell of the same row
